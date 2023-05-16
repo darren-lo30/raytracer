01796b01addc6751bf29e26e7403b2b9c5038d97
DarrenLo0530 <darren.lo.0530@gmail.com>
2023-05-16 01:12:58 -0400

    Removed gitignore

--- raytracing-gpu/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/darren/Documents/raytracing/raytracing-gpu
-# Build directory: /home/darren/Documents/raytracing/raytracing-gpu/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
